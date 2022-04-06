a04a61ee965f9f738ad0ebd86b8d19baee5031bd
Joxxx69 <santiago.gualotuna@hotmail.com>
2022-04-06 13:02:16 -0500

    cambios

--- 01-fundamentos/assets/js/class/sigleton.js
+++ 01-fundamentos/assets/js/class/sigleton.js
@@ -1,11 +1,19 @@
+// un singleton es una instancia unica de mi clase de manera global
+
 
 class Singleton{
     static #instancia;
     #nombre ='';
-    constructor(nombre = ''){
-        console.log(Singleton.#instancia);
-        this.#nombre = nombre;
 
+    constructor(nombre = ''){
+        console.log(!!Singleton.#instancia)
+        if (!!Singleton.#instancia){
+            console.log('1')
+            return Singleton.#instancia;
+        }
+        console.log('2')
+        Singleton.#instancia = this;
+        this.#nombre = nombre;
     }
     get getNombre(){
         return this.#nombre;
@@ -14,5 +22,18 @@ class Singleton{
 
 const instancia1 = new Singleton('IronMan');
 const instancia2 = new Singleton('Spiderman');
+const instancia3 = new Singleton('black Panther');
+ 
+console.log(`Nombre en la instacia1 es ${instancia1.getNombre}`);
+console.log(`Nombre en la instacia2 es ${instancia2.getNombre}`);
+console.log(`Nombre en la instacia3 es ${instancia3.getNombre}`);
 
-console.log(instancia1.getNombre)
+// console.log(instancia1.getNombre)
+
+
+/* 
+const a = undefined;
+console.log(a);     // undefined
+console.log(!a);   // true
+console.log(!!a); //  false
+ */
